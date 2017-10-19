e17700352ba5c43b6a0e939769fc5866f67769b4
Artem Kondratyuk <artem.kondratyuk.1995@gmail.com>
2017-10-19 17:13:07 +0300

    The Separation of Concerns

--- src/pages/app.php
+++ src/pages/app.php
@@ -14,13 +14,15 @@ function is_leap_year($year = null) {
 $routes = new Routing\RouteCollection();
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
     'year' => null,
-    '_controller' => function ($request) {
-        if (is_leap_year($request->attributes->get('year'))) {
-            return new Response('Yep, this is a leap year!');
-        }
+    '_controller' => 'Calendar\Controller\LeapYearController::indexAction',
+    //'_controller' => function ($request) {
+     //   if (is_leap_year($request->attributes->get('year'))) {
+     //       return new Response('Yep, this is a leap year!');
+     //   }
 
-        return new Response('Nope, this is not a leap year.');
-    }
+     //   return new Response('Nope, this is not a leap year.');
+  //  }
+
 )));
 
 return $routes;
