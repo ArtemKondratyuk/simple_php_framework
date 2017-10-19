2adbd2b735380c2c3e6d0f7079126cba79534273
Artem Kondratyuk <artem.kondratyuk.1995@gmail.com>
2017-10-19 16:16:57 +0300

    make templates even more readable

--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,6 +1,3 @@
-<?php $name = $request->get('name', 'World') ?>
+<!-- example.com/src/pages/hello.php -->
+Hello <?php echo htmlspecialchars(isset($name) ? $name : 'World', ENT_QUOTES, 'UTF-8') ?>
 
-Hello <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
-
-//$input = $request->get('name', 'World');
-//$response->setContent(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
